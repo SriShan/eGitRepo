ff20d20978212fa9156e30eedd1327cf9a5309e9
SriShan <ssrividya83@yahoo.co.in>
2016-05-12 00:02:17 -0400

    New Project through eGit

---
+++ GitProjects/src/com/home/egit/GitHelloWorld.java
@@ -0,0 +1,10 @@
+package com.home.egit;
+
+public class GitHelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HelloWorld from Git!!");
+	}
+
+}
